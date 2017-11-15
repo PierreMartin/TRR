74a54cfaeb7629aec88aafa536177ece0734a465
Pierre Martin <pierre.martin.prestataire@sfr.com>
2017-11-15 13:54:30 +0100

    refacto ssr

--- server/index.js
+++ server/index.js
@@ -1,37 +1,42 @@
-import path from 'path';
-import qs from 'qs';
-import Express from 'express';
-import React from 'react';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { renderToString } from 'react-dom/server';
-import counterApp from './../app/reducers';
-import App from './../app/components/App';
-import { fetchCoursesRequest } from './../app/api';
+/* eslint-disable no-console, no-use-before-define */
 
-const app = Express();
+import Express from 'express';
+import qs from 'qs';
+
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from '../webpack.config';
+
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { Provider } from 'react-redux';
+
+import configureStore from '../app/store/configureStore';
+import App from '../app/components/App';
+import { fetchCoursesRequest } from '../app/api';
+
+const app = new Express();
 const port = 3000;
 
-//Serve static files
-app.use('/static', Express.static('static'));
+// Use this middleware to set up hot module reloading via webpack.
+const compiler = webpack(webpackConfig);
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath }));
+app.use(webpackHotMiddleware(compiler));
 
-// This is fired every time the server side receives a request
-app.use(handleRender);
-
-
-// We are going to fill these out in the sections to follow
-function handleRender(req, res) {
+const handleRender = (req, res) => {
 	// Query our mock API asynchronously
 	fetchCoursesRequest((apiResult) => {
+		console.log('apiResult ', apiResult);
 		// Read the counter from the request, if provided
 		const params = qs.parse(req.query);
 		const counter = parseInt(params.counter, 10) || apiResult || 0;
 
 		// Compile an initial state
-		let preloadedState = { counter };
+		const preloadedState = { counter };
 
 		// Create a new Redux store instance
-		const store = createStore(counterApp, preloadedState);
+		const store = configureStore(preloadedState);
 
 		// Render the component to a string
 		const html = renderToString(
@@ -46,9 +51,12 @@ function handleRender(req, res) {
 		// Send the rendered page back to the client
 		res.send(renderFullPage(html, finalState));
 	});
-}
+};
 
-function renderFullPage(html, preloadedState) {
+// This is fired every time the server side receives a request
+app.use(handleRender);
+
+const renderFullPage = (html, preloadedState) => {
 	return `
     <!doctype html>
     <html>
@@ -56,16 +64,20 @@ function renderFullPage(html, preloadedState) {
         <title>Redux Universal Example</title>
       </head>
       <body>
-        <div id="root">${html}</div>
+        <div id="app">${html}</div>
         <script>
-          // WARNING: See the following for security issues around embedding JSON in HTML:
-          // http://redux.js.org/docs/recipes/ServerRendering.html#security-considerations
-          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
+          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\x3c')}
         </script>
         <script src="/assets/app.js"></script>
       </body>
     </html>
     `;
-}
+};
 
-app.listen(port);
+app.listen(port, (error) => {
+	if (error) {
+		console.error(error);
+	} else {
+		console.info(`==> 🌎  Listening on port ${port}. Open up http://localhost:${port}/ in your browser.`);
+	}
+});
